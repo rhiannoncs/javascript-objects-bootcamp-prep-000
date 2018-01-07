c2932dd5b16b517e88064a0904673c28dc0f6443
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-07 23:19:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,7 +7,8 @@ function updatePlaylist(playlist, artist, song) {
   return playlist
 }
 
-function removeFromPlaylist(list, artistName) {
-  delete list.artistName;
-  return list
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist
 }
+
