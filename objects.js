da356e2165561017fc59ac92fd3ae2189b53ee82
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-07 23:21:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -12,4 +12,4 @@ function removeFromPlaylist(playlist, artistName) {
   return playlist
 }
 
-console.log(removeFromPlaylist({ Josie: 'Poopy Doopy'}, Josie))
+console.log(removeFromPlaylist({ Josie: 'Poopy Doopy'}, "Josie"))
